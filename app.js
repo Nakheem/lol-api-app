777d3072b7830cfc89ee0fc28933c2c3cd493856
Nakheem <41401736+Nakheem@users.noreply.github.com>
2023-08-25 21:21:14 -0400

    Babelrc

--- app.js
+++ app.js
@@ -1,33 +1,16 @@
-import express from "express";
-import axios from "axios";
+const express = require("express");
+const axios = require("axios");
 
 const app = express();
 
 app.use(express.json())
 
-import {getNotes, getNote, createNote } from './database.js'
-
+//To-do move to env file
+const api_key = 'RGAPI-b502d60c-ff6e-40cc-a838-ebb8794f2363'
 
 app.get('/', (req, res) => {
     res.send('Welcome to the League of Legends API app!');
   });
-
-app.get("/notes", async (req,res) =>{
-    const notes = await getNotes()
-    res.send(notes)
-})
-
-app.get("/notes/:id", async(req, res) => {
-    const id = req.params.id
-    const note = await getNote(id)
-    res.send(note)
-})
-
-app.post("/notes", async (req, res) =>{
-    const {title, contents } = req.body
-    const note = await createNote(title, contents)
-    res.status(201).send(note)
-})
 
 app.use((err, req, res, next) => {
     console.error(err.stack)
@@ -42,13 +25,20 @@ app.get('/player/:summonerName', async (req, res) => {
     const { summonerName } = req.params;
     try {
 
-      const response = await axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=RGAPI-b502d60c-ff6e-40cc-a838-ebb8794f2363`, {
+      const response = await axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${api_key}`, {
 
       });
       const playerData = response.data;
       const puuid = playerData.puuid;
-      
       res.json(playerData);
+      
+      response = await axios.get(`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=20&api_key=${api_key}`)
+      playerGames = response.data;
+
+      for (games in playerGames){
+        
+      }
+      
     } catch (error) {
       console.log(error);
       res.status(500).json({ error: 'An error occurred while fetching player data' });
